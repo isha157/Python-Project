3afde62d507cbaf1995c955a3dd4725af07db6f9
ANISHA PANDA <pandaanisha890@gmil.com>
2026-03-02 16:26:55 +0530

    Removed secrets

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = "your_secret_key"
+    JWT_SECRET_KEY = "your_jwt_secret_key"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Whoisisha#2005@localhost/task_manager"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ main.py
@@ -0,0 +1,49 @@
+#step-1 install required libraries
+from twilio.rest import Client
+from datetime import datetime, timedelta
+import time
+
+#step-2 twilio credentials
+account_sid = "YOUR_TWILIO_ACCOUNT_SID"
+auth_token = "YOUR_TWILIO_AUTH_TOKEN"
+
+client = Client(account_sid, auth_token)
+#step-3 design send message function
+def send_whatsapp_message(recipient_number, message_body):
+    try:
+        message = client.messages.create(
+        from_= 'whatsapp:+14155238886',
+        body= message_body,
+        to=f'whatsapp:{recipient_number}'
+    )
+        print(f'Message sent successfully! Message SID{message.sid}')
+    except Exception as e:
+        print('An error occurred')
+
+#step-4 user input
+name = input('Enter the recipient name= ')
+recipient_number = input('Enter the recipient whatsappp number with country code (e.g. +12345)')
+message_body = input(f'enter the message you want to send to {name}')
+
+#step-5 parse date/time and calculate delay
+date_str = input('enter the date to send the message (YYYY-MM-DD): ')
+time_str  = input('enter the time to send the message (HH:MM in 24hour format): ')
+
+#datetime
+schedule_datetime = datetime.strptime(f'{date_str} {time_str}', "%Y-%m-%d %H:%M")
+current_datetime = datetime.now()
+
+#calculate delay
+time_difference = schedule_datetime - current_datetime
+delay_seconds = time_difference.total_seconds()
+
+if delay_seconds <=0:
+    print('The specified time is in the part. Please enter a future date nd time: ')
+else:
+    print(f'Message scheduled to be sent to {name} at {schedule_datetime}.')
+
+    #wait untile the scheduled time
+    time.sleep(delay_seconds) #1000
+
+    #send the message
+    send_whatsapp_message(recipient_number, message_body)
